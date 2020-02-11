476a37657c7fde9055534f393d62c93de8afff6b
umesh <umeshchandra.neeli@acheron-tech.com>
2020-02-11 17:53:03 +0530

    adding second folder

---
+++ second/changelog2.sql
@@ -0,0 +1,47 @@
+--liquibase formatted sql
+
+--changeset bobby:1
+create table test101 (
+id int primary key,
+name varchar(255)
+);
+
+
+
+
+
+
+
+--changeset bobby:3
+create table test103 (
+id int primary key,
+name varchar(255)
+);
+
+
+--changeset bobby:4
+create table test105 (
+id int primary key,
+name varchar(255)
+);
+
+
+--changeset bobby:6:yooooo
+create table test106 (
+id int primary key,
+name varchar(255)
+);
+
+
+--changeset bobby:dd:7
+create table test107 (
+id int primary key,
+name varchar(255)
+);
+
+
+--changeset bobby:8
+create table test108 (
+id int primary key,
+name varchar(255)
+);
